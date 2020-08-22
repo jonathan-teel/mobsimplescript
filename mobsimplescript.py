e58a4cd2e4fcc0359e511d58184cccc13e79275d
Jonathan <jonathan@jonathanteel.com>
2020-08-22 16:53:51 -0500

    updated http to https

--- mobsimplescript.py
+++ mobsimplescript.py
@@ -24,10 +24,10 @@ params = {}
 headers = requests.utils.default_headers()
 headers.update({
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-    'Origin': 'http://www.mobstar.cc'
+    'Origin': 'https://www.mobstar.cc'
 })
 req = requests.Session()
-url = "http://mobstar.cc"
+url = "https://mobstar.cc"
 rank = ''
 cash = ''
 plane = ''
